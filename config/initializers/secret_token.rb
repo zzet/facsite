83c8bc890376bf811ca64c436e3c460cf7916c83
Andrey Kumanyaev <me@zzet.org>
2013-10-14 21:40:48 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FacSite::Application.config.secret_key_base = 'f1f46e2d2bd0bac5661368463ea2227569e6304570faff8b89929ef287ff202b42c25da117dee708c64a30e70faa2df9f6548d7a8b47995edb91a894f544eb11'
